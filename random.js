eced8206a1fcbe6758423a142210e445104ac056
hurtazul <personal@juanantoniohurtado.es>
2015-07-06 18:03:14 +0200

    Version 2

--- random.js
+++ random.js
@@ -1,8 +1,10 @@
 //Math.random() devuelve número aletaorio entre 0 y 1
 var numero = Math.random();
 
+var str = " MENOR que 0.5";
+
 if (numero <= 0.5){
-  console.log('\n' + numero + ' MENOR que 0,5 \n');
+  console.log('\n' + numero + str '\n');
 }
 else {
   console.log('\n' + numero + ' MAYOR que 0.5 \n');
